04d964f6e4b1bd63f4d430fb631843db638498a8
Filip <fivanusec@gmail.com>
2020-09-20 11:34:36 +0200

    Changes 20.09.2020
    app\core\App.php - Redesign of the code in App.php, properly functioning and doing it's job
    
    app\init.php - fixing the default controller and adding paths for 404 and 500 files
    
    Addition of 404 and 500 pages in error folder in app\view
    
    vendor  - redone dumpautoload in composer

--- app/core/App.php
+++ app/core/App.php
@@ -2,8 +2,11 @@
 
 namespace app\core;
 
-use app\utils\Input;
 use Exception;
+use ReflectionClass;
+use ReflectionMethod;
+use app\utils\Input;
+use app\utils\Redirect;
 
 /**
  * App
@@ -15,60 +18,119 @@ use Exception;
 class App
 {
     /** @var mixed Default controller class */
-    private $controller = DEFAULT_CONTROLLER;
+    private $_class = DEFAULT_CONTROLLER;
 
     /** @var mixed Default controller action */
-    private $method = DEFAULT_CONTROLLER_ACTION;
+    private $_method = DEFAULT_CONTROLLER_ACTION;
 
     /** @var array Params that pass to Controller */
-    private $params = [];
+    private $_params = [];
+
+    /**
+     * Construct: Processes the app by parses the URL and sets the class, class
+     * method and method parameters.
+     * @access public
+     * @since 0.1[ALPHA]
+     */
 
     public function __construct()
     {
-        $url = $this->parseUrl();
-        $this->getController($url);
-        $this->getMethod($url);
-        $this->getParams($url);
-    }
-
-    public function getMethod($url)
-    {
-        if (isset($url[1])) {
-            if (method_exists($this->controller, $url[1])) {
-                $this->method = $url[1];
-            }
+        $this->_parseURL();
+        try {
+            $this->_getClass();
+            $this->_getMethod();
+            $this->_getParams();
+        } catch (Exception $e) {
+            Redirect::to(404);
         }
     }
 
-    public function getController($url)
+    /**
+     * Get Class: Checks if the first URL element is set and not empty, replacing
+     * the default controller class string if the given class exists. If the class
+     * exists, said string is replaced with a new instance of it.
+     * @access private
+     * @return void
+     * @since 0.1[ALPHA]
+     * @throws Exception
+     */
+
+    private function _getClass()
     {
-        if (isset($url[0])) {
-            if (file_exists(CONTROLLERS . $url[0] . '.php')) {
-                $this->controller = $url[0];
-            }
+        if (isset($this->_params[0]) and !empty($this->_params[0])) {
+            $this->_class = NAMESPACE_CONTROLLER . ucfirst(strtolower($this->_params[0]));
         }
-        require_once CONTROLLERS . $this->controller . '.php';
-        $this->controller = NAMESPACE_CONTROLLER . ucfirst(strtolower($this->controller));
-        $this->controller = new $this->controller;
+
+        if (!class_exists($this->_class)) {
+            throw new Exception("The controller {$this->_class} does not exist!");
+        }
+        unset($this->_params[0]);
+        $this->_class = new $this->_class;
     }
 
-    private function getParams($url)
-    {
-        unset($url[0]);
-        unset($url[1]);
-        $this->params = $url ? array_values($url) : [];
-    }
+    /**
+     * Get Method: Checks if the second URL element is set and not empty,
+     * replacing the default controller method string if the given action is a
+     * valid class method.
+     * @access private
+     * @return void
+     * @since 0.1[ALPHA]
+     * @throws Exception
+     */
 
-    public function parseUrl()
+    private function _getMethod()
     {
-        if ($url = Input::get("url")) {
-            return $url = explode('/', filter_var(rtrim($url, '/'), FILTER_SANITIZE_URL));
+        if (isset($this->_params[1]) and !empty($this->_params[1])) {
+            $this->_method = $this->_params[1];
+            unset($this->_params[1]);
+        }
+
+        if (!(new ReflectionClass($this->_class))->hasMethod($this->_method)) {
+            throw new Exception("The controller method {$this->_method} does not exist!");
+        }
+
+        if (!(new ReflectionMethod($this->_class, $this->_method))->isPublic()) {
+            throw new Exception("The controller method {$this->_method} is not accessible!");
         }
     }
+
+    /**
+     * Get Params: Checks if the URL has any remaining elements, setting the 
+     * parameters as a rebase of it if true or an empty array if false.
+     * @access private
+     * @return void
+     * @since 0.1[ALPHA]
+     */
+
+    private function _getParams()
+    {
+        $this->_params = $this->_params ? array_values($this->_params) : [];
+    }
+
+    /**
+     * Parse URL: Gets the different parts of the URL string.
+     * @access private
+     * @return void
+     * @since 0.1[ALPHA]
+     */
+
+    private function _parseURL()
+    {
+        if (($url = Input::get("url"))) {
+            $this->_params = explode("/", filter_var(rtrim($url, "/"), FILTER_SANITIZE_URL));
+        }
+    }
+
+    /**
+     * Run: Calls the controller class and method with parameters.
+     * @access public
+     * @return void
+     * @since 0.1[ALPHA]
+     */
 
     public function run()
     {
-        call_user_func_array([$this->controller, $this->method], $this->params);
+        call_user_func_array([$this->_class, $this->_method], $this->_params);
     }
 }
 
---
+++ app/views/error/500.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <title>500</title>
+</head>
+
+<style>
+    body {
+        background: #dedede;
+    }
+
+    .page-wrap {
+        min-height: 100vh;
+    }
+</style>
+
+<body>
+    <div class="page-wrap d-flex flex-row align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12 text-center">
+                    <span class="display-1 d-block">500</span>
+                    <div class="mb-4 lead">There was an internal server error! Please contact support!</div>
+                    <a href="http://organize.hr/public/" class="btn btn-link">Back to Home</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
